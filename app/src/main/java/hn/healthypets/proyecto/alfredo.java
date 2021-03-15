e9f1f93ba06d7dff6cdfe80957c8b4f9836c0087
natbai1106 <natbai1106@gmail.com>
2021-03-14 23:57:39 -0600

    ESTA ES LA PRIMERA PRUEBA

---
+++ app/src/main/java/hn/healthypets/proyecto/alfredo.java
@@ -0,0 +1,14 @@
+package hn.healthypets.proyecto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class alfredo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alfredo);
+    }
+}
