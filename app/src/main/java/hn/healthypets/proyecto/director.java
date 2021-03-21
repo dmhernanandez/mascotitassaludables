eee3fc32a8968d809a0b9d31da852d97de0e86f0
SumomoAigasaki <sara_osorto12@hotmail.com>
2021-03-21 01:23:52 -0600

    21-03-2021 Pantalla Inicio Y Crear perfil

---
+++ app/src/main/java/hn/healthypets/proyecto/director.java
@@ -0,0 +1,14 @@
+package hn.healthypets.proyecto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class director extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_director);
+    }
+}
