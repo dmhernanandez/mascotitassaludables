9602c317d76b50bafcbbccd15468a37a2d627f3d
Sarah Osorto <sumomoaigasaki@gmail.com>
2021-04-02 22:25:36 -0600

    Avance sara 02/04/2021 10:24pm Termine los dise;os, iconos, edite el mainactivity

--- app/src/main/java/hn/healthypets/proyecto/MainActivity.java
+++ app/src/main/java/hn/healthypets/proyecto/MainActivity.java
@@ -15,40 +15,55 @@ public class MainActivity extends AppCompatActivity {
         Button boton = findViewById(R.id.button);
         Button nuevo = findViewById(R.id.nuevo);
         Button credencial = findViewById(R.id.btnCredencial);
-        Button vacuna= findViewById(R.id.btnCredencial2);
+        Button vacuna= findViewById(R.id.btnVacunas);
+        Button desparacitante=findViewById(R.id.btnDesparacitante);
+        Button historial=findViewById(R.id.btnHistorial);
+        Button medicamento=findViewById(R.id.btnMedicamento);
+        Button agenda=findViewById(R.id.btnAgenda);
+        Button actividades=findViewById(R.id.btnActividades);
 
         boton.setOnClickListener(v -> {
-
             Intent intent =new Intent(this,CreacionPerfiles.class);
-
             startActivity(intent);
 
         });
 
         nuevo.setOnClickListener(v -> {
-
             Intent intent2 =new Intent(this, Director.class);
-
             startActivity(intent2);
-
-
         });
 
         credencial.setOnClickListener(v -> {
-
             Intent intent3 =new Intent(this, Perfiles.class);
-
             startActivity(intent3);
-
-
         });
 
         vacuna.setOnClickListener(v -> {
-
             Intent intent4 =new Intent(this, Vacunas.class);
-
             startActivity(intent4);
-
         });
+
+        desparacitante.setOnClickListener(v -> {
+            Intent intent5 =new Intent(this, Desparacitante.class);
+            startActivity(intent5);
+        });
+        historial.setOnClickListener(v -> {
+            Intent intent6=new Intent(this, Historial.class);
+            startActivity(intent6);
+        });
+
+        medicamento.setOnClickListener(v -> {
+            Intent intent7 =new Intent(this, Medicamento.class);
+            startActivity(intent7);
+        });
+        agenda.setOnClickListener(v -> {
+            Intent intent8 =new Intent(this, Agenda.class);
+            startActivity(intent8);
+        });
+        actividades.setOnClickListener(v -> {
+            Intent intent9 =new Intent(this, Actividades.class);
+            startActivity(intent9);
+        });
+
     }
 }
