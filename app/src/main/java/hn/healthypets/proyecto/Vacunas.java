797384f6e28671808181d860e8d7582e54b40c0c
Sarah Osorto <sumomoaigasaki@gmail.com>
2021-04-06 01:43:50 -0600

    Avance Sara 06/04/2021 1.45 am Arregle el codigo de Vacunas

--- app/src/main/java/hn/healthypets/proyecto/Vacunas.java
+++ app/src/main/java/hn/healthypets/proyecto/Vacunas.java
@@ -1,4 +1,14 @@
 package hn.healthypets.proyecto;
 
-public class Vacunas {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Vacunas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vacunas);
+    }
 }
