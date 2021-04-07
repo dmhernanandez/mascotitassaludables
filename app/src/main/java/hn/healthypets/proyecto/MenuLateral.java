a55e3f4fc2b752d58c800435d9466a10df443025
Sarah Osorto <sumomoaigasaki@gmail.com>
2021-04-07 00:39:18 -0600

    Avance sara 7/04/2021 12.20 am Pude hacer que aparezca la bienvenida en el menu

--- app/src/main/java/hn/healthypets/proyecto/MenuLateral.java
+++ app/src/main/java/hn/healthypets/proyecto/MenuLateral.java
@@ -26,14 +26,7 @@ public class MenuLateral extends AppCompatActivity {
         setContentView(R.layout.activity_menu_lateral);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-//        FloatingActionButton fab = findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
+
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
@@ -41,7 +34,7 @@ public class MenuLateral extends AppCompatActivity {
 
 
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+                R.id.nav_home, R.id.nav_perfiles, R.id.nav_slideshow)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
--- app/src/main/java/hn/healthypets/proyecto/ui/home/HomeFragment.java
+++ app/src/main/java/hn/healthypets/proyecto/ui/home/HomeFragment.java
@@ -1,9 +1,12 @@
 package hn.healthypets.proyecto.ui.home;
 
+import android.app.FragmentManager;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,23 +15,33 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import hn.healthypets.proyecto.CreacionPerfiles;
 import hn.healthypets.proyecto.R;
+
+import android.app.FragmentTransaction;
+import hn.healthypets.proyecto.ui.gallery.PerfilesFragment;
 
 public class HomeFragment extends Fragment {
 
-    private HomeViewModel homeViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        View vista = inflater.inflate(R.layout.fragment_home, container, false);
+
+        Button aniadir=vista.findViewById(R.id.btnAniadirMascota);
+
+
+        aniadir.setOnClickListener(new View.OnClickListener() {
+                          //        PARA ABRIR DE UN FRAGMENTE A UNA ACTIVITY
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(),CreacionPerfiles.class);
+                startActivity(intent);
             }
+
         });
-        return root;
+
+
+        return vista;
     }
 }
