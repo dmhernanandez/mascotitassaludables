915e8a2ea5f2a6ced81498377be2a0bff64ce7c6
natbai1106 <natbai1106@gmail.com>
2021-04-09 01:31:38 -0600

    Avance alfredo 09-04-21 Agregar permisos para usar camara y acceso a galeria, FUNCIONAL

--- app/src/main/java/hn/healthypets/proyecto/Vacunas.java
+++ app/src/main/java/hn/healthypets/proyecto/Vacunas.java
@@ -18,7 +18,6 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Log;
-import android.view.OrientationEventListener;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
@@ -26,6 +25,7 @@ import android.widget.Toast;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.IllegalFormatCodePointException;
 
 public class Vacunas extends AppCompatActivity {
     ImageButton buscarImagen;
@@ -35,6 +35,9 @@ public class Vacunas extends AppCompatActivity {
 
     private static final int REQUEST_PERMISSION_CODE = 100;
     private static final int REQUEST_IMAGE_GALLERY = 101;
+    private static final int REQUEST_PERMISSION_CAMERA = 102;
+    private static final int REQUEST_IMAGE_CAMERA = 103;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,16 +49,15 @@ public class Vacunas extends AppCompatActivity {
 
         imgFotoVacuna = findViewById(R.id.imgComprobacionVacunas);
         buscarImagen = findViewById(R.id.imgbtnBuscarFotosV);
+
         buscarImagen.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                    if (ActivityCompat.checkSelfPermission(Vacunas.this, Manifest.permission.READ_EXTERNAL_STORAGE)
-                            == PackageManager.PERMISSION_GRANTED) {
+                    if (ActivityCompat.checkSelfPermission(Vacunas.this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
                         openGallery();
                     } else {
-                        ActivityCompat.requestPermissions(Vacunas.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
-                                REQUEST_PERMISSION_CODE);
+                        ActivityCompat.requestPermissions(Vacunas.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_PERMISSION_CODE);
                     }
                 } else {
                     openGallery();
@@ -66,20 +68,28 @@ public class Vacunas extends AppCompatActivity {
         btnTomarFotos.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                abrirCamara();
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                    if (ActivityCompat.checkSelfPermission(Vacunas.this, Manifest.permission.CAMERA)==PackageManager.PERMISSION_GRANTED){
+                        goToCamera();
+                    }else {
+                        ActivityCompat.requestPermissions(Vacunas.this, new String[]{Manifest.permission.CAMERA}, REQUEST_PERMISSION_CAMERA);
+                    }
+                }else {
+                    goToCamera();
+                }
             }
         });
     }
 
     private void openGallery() {
-        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-        intent.setType("image/*");
-        startActivityForResult(intent, REQUEST_IMAGE_GALLERY);
+        Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        galleryIntent.setType("image/*");
+        startActivityForResult(galleryIntent, REQUEST_IMAGE_GALLERY);
     }
 
-    private void abrirCamara() {
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (intent.resolveActivity(getPackageManager()) != null) {
+    private void goToCamera() {
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (cameraIntent.resolveActivity(getPackageManager()) != null) {
             File imagenArchivo = null;
             try {
                 imagenArchivo = crearImagen();
@@ -88,8 +98,8 @@ public class Vacunas extends AppCompatActivity {
             }
             if (imagenArchivo != null) {
                 Uri fotoUri = FileProvider.getUriForFile(this, "hn.healthypets.proyecto.fileprovider", imagenArchivo);
-                intent.putExtra(MediaStore.EXTRA_OUTPUT, fotoUri);
-                startActivityForResult(intent, 1);
+                cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, fotoUri);
+                startActivityForResult(cameraIntent, REQUEST_IMAGE_CAMERA);
             }
         }
     }
@@ -100,24 +110,25 @@ public class Vacunas extends AppCompatActivity {
         if (requestCode == REQUEST_IMAGE_GALLERY) {
             if (resultCode == Activity.RESULT_OK && data != null) {
                 Uri photo = data.getData();
+                photo.getPath();
+                Log.i("foto", photo.getPath());
                 imgFotoVacuna.setImageURI(photo);
             } else {
                 Log.i("TAG", "Result: " + resultCode);
                 Toast.makeText(this, "No selecciono ninguna foto", Toast.LENGTH_LONG).show();
             }
         } else {
-            if (requestCode == 1 && resultCode == RESULT_OK) {
-                Log.i("error", "entro");
+            if (requestCode == REQUEST_IMAGE_CAMERA && resultCode == RESULT_OK) {
 //            Bundle extras = data.getExtras();
 //            Bitmap imgBitmap = (Bitmap)extras.get("data")
                 Bitmap imgBitmap = BitmapFactory.decodeFile(rutaImagen);
                 imgFotoVacuna.setImageBitmap(imgBitmap);
-            } else {
-                if (resultCode == RESULT_OK) {
-                    Uri path = data.getData();
-                    imgFotoVacuna.setImageURI(path);
-                }
-            }
+            } //else {
+//                if (resultCode == RESULT_OK) {
+//                    Uri path = data.getData();
+//                    imgFotoVacuna.setImageURI(path);
+//                }
+//            }
         }
     }
 
@@ -130,11 +141,18 @@ public class Vacunas extends AppCompatActivity {
                 Toast.makeText(this, "Necesita todos los permisos", Toast.LENGTH_LONG).show();
             }
         }
+        if (requestCode==REQUEST_PERMISSION_CAMERA){
+            if (permissions.length >0 && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+               goToCamera();
+            }else {
+                Toast.makeText(this, "Necesita habilitar los permisos", Toast.LENGTH_LONG).show();
+            }
+        }
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 
     private File crearImagen() throws IOException {
-        String nombreImagen = "fotoVacuna_";
+        String nombreImagen = "fotoVacunaCamara_";
         File directorio = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
         File imagen = File.createTempFile(nombreImagen, ".jpg", directorio);
         rutaImagen = imagen.getAbsolutePath();
