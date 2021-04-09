881ab3ac770edc23350beed2e1f1853f95fd0540
natbai1106 <natbai1106@gmail.com>
2021-04-08 22:34:10 -0600

    Avance Alfredo 08-04-21 10:33pm Agregados permisos de uso de galeria

--- app/src/main/java/hn/healthypets/proyecto/Vacunas.java
+++ app/src/main/java/hn/healthypets/proyecto/Vacunas.java
@@ -1,13 +1,19 @@
 package hn.healthypets.proyecto;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.FileProvider;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
@@ -16,6 +22,7 @@ import android.view.OrientationEventListener;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.io.File;
 import java.io.IOException;
@@ -25,6 +32,9 @@ public class Vacunas extends AppCompatActivity {
     ImageButton btnTomarFotos;
     ImageView imgFotoVacuna;
     String rutaImagen;
+
+    private static final int REQUEST_PERMISSION_CODE = 100;
+    private static final int REQUEST_IMAGE_GALLERY = 101;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,7 +49,17 @@ public class Vacunas extends AppCompatActivity {
         buscarImagen.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                cargarImagen();
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    if (ActivityCompat.checkSelfPermission(Vacunas.this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                            == PackageManager.PERMISSION_GRANTED) {
+                        openGallery();
+                    } else {
+                        ActivityCompat.requestPermissions(Vacunas.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                                REQUEST_PERMISSION_CODE);
+                    }
+                } else {
+                    openGallery();
+                }
             }
         });
 
@@ -51,10 +71,10 @@ public class Vacunas extends AppCompatActivity {
         });
     }
 
-    private void cargarImagen() {
-        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-        intent.setType("image/");
-        startActivityForResult(intent.createChooser(intent, "Seleccione la aplicacion"), 10);
+    private void openGallery() {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("image/*");
+        startActivityForResult(intent, REQUEST_IMAGE_GALLERY);
     }
 
     private void abrirCamara() {
@@ -77,18 +97,40 @@ public class Vacunas extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == 1 && resultCode == RESULT_OK) {
-            Log.i("error", "entro");
+        if (requestCode == REQUEST_IMAGE_GALLERY) {
+            if (resultCode == Activity.RESULT_OK && data != null) {
+                Uri photo = data.getData();
+                imgFotoVacuna.setImageURI(photo);
+            } else {
+                Log.i("TAG", "Result: " + resultCode);
+                Toast.makeText(this, "No selecciono ninguna foto", Toast.LENGTH_LONG).show();
+            }
+        } else {
+            if (requestCode == 1 && resultCode == RESULT_OK) {
+                Log.i("error", "entro");
 //            Bundle extras = data.getExtras();
 //            Bitmap imgBitmap = (Bitmap)extras.get("data")
-            Bitmap imgBitmap = BitmapFactory.decodeFile(rutaImagen);
-            imgFotoVacuna.setImageBitmap(imgBitmap);
-        } else {
-            if (resultCode == RESULT_OK) {
-                Uri path = data.getData();
-                imgFotoVacuna.setImageURI(path);
+                Bitmap imgBitmap = BitmapFactory.decodeFile(rutaImagen);
+                imgFotoVacuna.setImageBitmap(imgBitmap);
+            } else {
+                if (resultCode == RESULT_OK) {
+                    Uri path = data.getData();
+                    imgFotoVacuna.setImageURI(path);
+                }
             }
         }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_PERMISSION_CODE) {
+            if (permissions.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+            } else {
+                Toast.makeText(this, "Necesita todos los permisos", Toast.LENGTH_LONG).show();
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 
     private File crearImagen() throws IOException {
