2b73bfe9621ecab77eaf6578a513ece5f781fc41
natbai1106 <natbai1106@gmail.com>
2021-04-12 23:13:51 -0600

    Avance Alfredo, actualizcion de vacunas y xml de vacunas y desparasitante

--- app/src/main/java/hn/healthypets/proyecto/Desparacitante.java
+++ app/src/main/java/hn/healthypets/proyecto/Desparacitante.java
@@ -1,36 +1,112 @@
 package hn.healthypets.proyecto;
 
+import android.app.DatePickerDialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import hn.healthypets.proyecto.Utilidades.DateTime;
+import hn.healthypets.proyecto.Utilidades.Validacion;
+import hn.healthypets.proyecto.database.DataBase;
+import hn.healthypets.proyecto.database.SingletonDB;
+import hn.healthypets.proyecto.modelos_mascotitas_saludables.Constantes;
+
 public class Desparacitante extends AppCompatActivity {
 
+    MetodosImagenes metodosImagenes = new MetodosImagenes();
     private EditText edtFechaApliDesp;
     private EditText edtNombreDespa;
     private EditText edtPesoDespa;
     private EditText edtObserDespa;
     private Button btnListo;
     private Button btnCancel;
+    private DataBase instanciaDB;
+    private DateTime fechaHora;
+    private int dia, mes, anio;
+
+    /**
+     * Se utilizan para validar que tipo de accion se realizara en la actividad, estos datos se reciben del intent
+     **/
+    private int accion;
+    private String especie = "";
+    private String raza;
+    private Bitmap bitmapImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_desparacitante);
+        //Inicializamos todos los elementos
+        init();
 
+        edtFechaApliDesp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Utilizamos este metodo par obtenener los datos
+                DatePickerDialog dialogoFecha = new DatePickerDialog(Desparacitante.this, (view, year, month, dayOfMonth) ->
+                        edtFechaApliDesp.setText(fechaHora.formato(dayOfMonth, month, year)), anio, mes, dia);
+                dialogoFecha.show();
+            }
+        });
+
+        btnListo.setOnClickListener((v) -> {
+            boolean validacion = Validacion.fieldsAreNotEmpty(
+                    edtFechaApliDesp.getText().toString(),
+                    edtNombreDespa.getText().toString(),
+                    edtPesoDespa.getText().toString());
+            if (validacion) {
+                metodosImagenes.checkPermissionStorage(Desparacitante.this);
+                Toast.makeText(Desparacitante.this, "ESTAN TODOS LOS CAMPOS LLENAMOS", Toast.LENGTH_SHORT).show();
+            } else
+
+                Toast.makeText(Desparacitante.this, "HAY campos vacios", Toast.LENGTH_SHORT).show();
+        });
+
+
+    }
+    private void init()
+    {
         edtFechaApliDesp=findViewById(R.id.edtFechaAplicacionDesparacitante);
         edtNombreDespa=findViewById(R.id.edtNombreDesparacitante);
         edtPesoDespa=findViewById(R.id.edtPesoDesparacitante);
         edtObserDespa=findViewById(R.id.edtObservacionesDesparacitante);
         btnListo=findViewById(R.id.btnListoDesparacitante);
         btnCancel=findViewById(R.id.btnCancelarDesparacitante);
+        fechaHora = new DateTime();
+
+        /** Se obtiene una instancia de la base de datos*/
+        instanciaDB = SingletonDB.getDatabase(this);
+
+        /**Obtemos datos del Intent y determinamos si es una actualizacion o una insercion, estos valores se optienen con el */
+        Intent intentValues= getIntent();
+        accion=intentValues.getIntExtra(Constantes.TAG_ACCION,Constantes.ACTUALIZAR);
+        if(accion==Constantes.GUARDAR){
+
+
+            //Recuperamos el valor de la fecha por defecto que es la fecha actual
+            dia=DateTime.diaDelMes;
+            mes=DateTime.mes;
+            anio=DateTime.anio;
+
+        }
+        else if(accion==Constantes.ACTUALIZAR)
+        {
+            String fecha1="15-03-2021";
+            /** Si es una actualización se debe parsear la fecha guadarda previamente para colocarla en variables de fecha
+             * para asignarlo y luego asignarla al input*/
+            String [] fecha=fecha1.split("-");
+            dia=Integer.parseInt(fecha[0]);
+            mes=Integer.parseInt(fecha[1])-1;
+            anio= Integer.parseInt(fecha[2]);
+
+        }
+        edtFechaApliDesp.setText(fechaHora.formato(dia,mes,anio));
+        accion=Constantes.ACTUALIZAR;
     }
-
-    //Primero relacionar los elementos
-    //On click en el boton guardar
-    // Validar que los campos obligatorios no esten vacios
-    // si estan vacios va mandar un mensaje
-
 }
--- app/src/main/java/hn/healthypets/proyecto/Medicamento.java
+++ app/src/main/java/hn/healthypets/proyecto/Medicamento.java
@@ -2,14 +2,25 @@ package hn.healthypets.proyecto;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Space;
 import android.widget.Spinner;
+import android.widget.Toast;
+
+import hn.healthypets.proyecto.Utilidades.DateTime;
+import hn.healthypets.proyecto.Utilidades.Validacion;
+import hn.healthypets.proyecto.database.DataBase;
+import hn.healthypets.proyecto.database.SingletonDB;
+import hn.healthypets.proyecto.modelos_mascotitas_saludables.Constantes;
 
 public class Medicamento extends AppCompatActivity {
-    private EditText edtNombreDesparacitante2;
+
+    MetodosImagenes metodosImagenes = new MetodosImagenes();
+    private EditText edtNombreMedicamento;
     private EditText edtNumeroDosis;
     private Spinner spiDosis;
     private EditText edtCadaDosis;
@@ -19,13 +30,52 @@ public class Medicamento extends AppCompatActivity {
     private EditText edtIndicacionesMedicamento;
     private Button btnListo;
     private Button btnCancel;
+    private DataBase instanciaDB;
+    private DateTime fechaHora;
+    private int dia, mes, anio;
+
+    /**
+     * Se utilizan para validar que tipo de accion se realizara en la actividad, estos datos se reciben del intent
+     **/
+    private int accion;
+    private String especie = "";
+    private String raza;
+    private Bitmap bitmapImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_medicamento);
+        //Inicializamos todos los elementos
+        init();
 
-        edtNombreDesparacitante2=findViewById(R.id.edtNombreDesparacitante2);
+        edtFechaMedicamento.setOnClickListener((v)->{
+            //Utilizamos este metodo par obtenener los datos
+            DatePickerDialog dialogoFecha = new DatePickerDialog(Medicamento.this, (view, year, month, dayOfMonth) ->
+                    edtFechaMedicamento.setText(fechaHora.formato(dayOfMonth, month, year)), anio, mes, dia);
+            dialogoFecha.show();
+        });
+
+        btnListo.setOnClickListener((v) -> {
+            boolean validacion = Validacion.fieldsAreNotEmpty(
+                    edtNombreMedicamento.getText().toString(),
+                    edtNumeroDosis.getText().toString(),
+//                    spiDosis.getText().toString(),
+                    edtCadaDosis.getText().toString(),
+                    edtPorDosis.getText().toString(),
+                    edtFechaMedicamento.getText().toString(),
+                    edtHoraMedicamento.getText().toString());
+            if (validacion) {
+                metodosImagenes.checkPermissionStorage(Medicamento.this);
+                Toast.makeText(Medicamento.this, "ESTAN TODOS LOS CAMPOS LLENAMOS", Toast.LENGTH_SHORT).show();
+            } else
+
+                Toast.makeText(Medicamento.this, "HAY campos vacios", Toast.LENGTH_SHORT).show();
+        });
+    }
+    private void init()
+    {
+        edtNombreMedicamento =findViewById(R.id.edtNombreMedicamento);
         edtNumeroDosis=findViewById(R.id.edtNumeroDosis);
         spiDosis=findViewById(R.id.spiDosis);
         edtCadaDosis=findViewById(R.id.edtCadaDosis);
@@ -33,7 +83,36 @@ public class Medicamento extends AppCompatActivity {
         edtFechaMedicamento=findViewById(R.id.edtFechaMedicamento);
         edtHoraMedicamento=findViewById(R.id.edtHoraMedicamento);
         edtIndicacionesMedicamento=findViewById(R.id.edtIndicacionesMedicamento);
-        btnListo=findViewById(R.id.btnListoDesparacitante2);
-        btnCancel=findViewById(R.id.btnCancelarDesparacitante2);
+        btnListo=findViewById(R.id.btnListoMedicamento);
+        btnCancel=findViewById(R.id.btnCancelarMedicamento);
+        fechaHora = new DateTime();
+
+        /** Se obtiene una instancia de la base de datos*/
+        instanciaDB = SingletonDB.getDatabase(this);
+        /**Obtemos datos del Intent y determinamos si es una actualizacion o una insercion, estos valores se optienen con el */
+        Intent intentValues= getIntent();
+        accion=intentValues.getIntExtra(Constantes.TAG_ACCION,Constantes.ACTUALIZAR);
+        if(accion==Constantes.GUARDAR){
+
+
+            //Recuperamos el valor de la fecha por defecto que es la fecha actual
+            dia=DateTime.diaDelMes;
+            mes=DateTime.mes;
+            anio=DateTime.anio;
+
+        }
+        else if(accion==Constantes.ACTUALIZAR)
+        {
+            String fecha1="15-03-2021";
+            /** Si es una actualización se debe parsear la fecha guadarda previamente para colocarla en variables de fecha
+             * para asignarlo y luego asignarla al input*/
+            String [] fecha=fecha1.split("-");
+            dia=Integer.parseInt(fecha[0]);
+            mes=Integer.parseInt(fecha[1])-1;
+            anio= Integer.parseInt(fecha[2]);
+
+        }
+        edtFechaMedicamento.setText(fechaHora.formato(dia,mes,anio));
+        accion=Constantes.ACTUALIZAR;
     }
 }
