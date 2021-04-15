b588f720283e6b17e690fd220de2129b9b8ab962
Sarah Osorto <sumomoaigasaki@gmail.com>
2021-04-14 23:29:13 -0600

    Avance sara 14/04/2021 10.25 pm Arregle miquis

--- app/src/main/java/hn/healthypets/proyecto/MenuLateral.java
+++ app/src/main/java/hn/healthypets/proyecto/MenuLateral.java
@@ -72,7 +72,7 @@ public class MenuLateral extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_lateral, menu);
+//        getMenuInflater().inflate(R.menu.menu_lateral, menu);
         return true;
     }
 
